e6e3167f5a134954a7aed816036a54054c0201b6
Francesco Sullo <sullof@gmail.com>
2022-02-02 11:36:13 -0800

    Fix getStakeWeight name

--- contracts/pools/SyndicatePoolBase.sol
+++ contracts/pools/SyndicatePoolBase.sol
@@ -482,7 +482,7 @@ abstract contract SyndicatePoolBase is IPool, SyndicateAware, ReentrancyGuard {
     uint64 lockUntil = _lockUntil;
 
     // stake weight formula rewards for locking
-    uint256 stakeWeight = getStakeWight(lockUntil - lockFrom, addedAmount);
+    uint256 stakeWeight = getStakeWeight(lockUntil - lockFrom, addedAmount);
 
     // makes sure stakeWeight is valid
     assert(stakeWeight > 0);
@@ -510,7 +510,7 @@ abstract contract SyndicatePoolBase is IPool, SyndicateAware, ReentrancyGuard {
     emit Staked(msg.sender, _staker, _amount);
   }
 
-  function getStakeWight(uint256 lockedTime, uint256 addedAmount) public pure returns (uint256) {
+  function getStakeWeight(uint256 lockedTime, uint256 addedAmount) public pure returns (uint256) {
     return ((lockedTime * WEIGHT_MULTIPLIER) / 365 days + WEIGHT_MULTIPLIER) * addedAmount;
   }
 
