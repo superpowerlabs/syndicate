5e46526ce7fb8039bc065e89adeb0fe76386e5c5
Francesco Sullo <sullof@gmail.com>
2022-06-01 17:59:23 -0700

    Showing that the weight is kept

--- scripts/deploy-pool.js
+++ scripts/deploy-pool.js
@@ -29,7 +29,8 @@ async function main() {
   console.log('Deploying SyndicatePoolFactory')
   const PoolFactory = await ethers.getContractFactory("SyndicatePoolFactory")
   const blockNumberFactoryConstructor = (await ethers.provider.getBlockNumber())
-      + (chainId === 1 ? delay : chainId === 42 ? 40 : 1)
+      // + 1
+      //(chainId === 1 ? delay : chainId === 42 ? 40 : 1)
 
   const poolFactory = await PoolFactory.deploy(
       synAddress,
